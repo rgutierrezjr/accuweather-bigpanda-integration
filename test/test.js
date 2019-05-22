bfcca02845c727877a289bc9f1b85cfee7c588db
ruben <ruben@bigboston.io>
2019-05-21 20:55:32 -0500

    added successful integration function call

--- test/test.js
+++ test/test.js
@@ -290,3 +290,22 @@ describe('BigPanda: unsuccessful notification', () => {
     })
 
 })
+
+describe('Integrate: successful integration', () => {
+
+    before(() => {
+        nock('http://dataservice.accuweather.com')
+            .get(`/currentconditions/v1/710949?apikey=J8vUGFMEx3k19kGjqvcjhsJbKyOHCbsA`)
+            .reply(200, accuweather_response_pass);
+
+        nock('https://api.bigpanda.io')
+            .post('/data/v2/alerts')
+            .reply(201, bigpanda_response_pass);
+    })
+
+    it('Successful current condition retrieval and notification', () => {
+        return integration.integrate('New York', '710949', (error, resultMessage) => {
+            expect(resultMessage).to.equal('Success: BigPanda notified.')
+        })
+    })
+})
